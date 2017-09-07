515f932d2bd0b035b8e4c6589d5b7a3bb035a817
Stephen Russell <stephenjohnrussell@me.com>
2017-09-07 17:54:46 +0100

    Random Colours

--- avatarist.go
+++ avatarist.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"math/rand"
+	"time"
 )
 
 func main() {
 	avatar := initAvatar(200, 200, 20, 20, 2)
+	fmt.Println(avatar.String())
+	avatar.GenerateRandomColours(98, 179, 229, 255, 175, 28)
 	fmt.Println(avatar.String())
 }
 
@@ -20,6 +24,7 @@ type Avatar struct {
 	NumCols    int
 	Img        *image.RGBA
 	Colours    []color.RGBA
+	BaseColour color.RGBA
 }
 
 //Set avatar sizes and init the image & colours
@@ -37,6 +42,19 @@ func initAvatar(width, height, blockSize, borderSize, numCols int) Avatar {
 
 func (a *Avatar) String() string {
 	return fmt.Sprintf("Width \t\t:: %v\nHeight \t\t:: %v\nBlockSize \t:: %v\n"+
-		"BorderSize \t:: %v\nNumCols \t:: %v\n", a.Width, a.Height, a.BlockSize,
-		a.BorderSize, a.NumCols)
+		"BorderSize \t:: %v\nNumCols \t:: %v\nColours \t:: %v\nBaseColour \t:: %v\n", a.Width, a.Height, a.BlockSize,
+		a.BorderSize, a.NumCols, a.Colours, a.BaseColour)
+}
+
+//GenerateRandomColours used in avatar.
+func (a *Avatar) GenerateRandomColours(red, green, blue, alpha, offset, randomAmount int) {
+	a.BaseColour = color.RGBA{uint8(red), uint8(green), uint8(blue), uint8(alpha)}
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < a.NumCols; i++ {
+		r := uint8(rand.Intn(red) + rand.Intn(randomAmount) + offset)
+		g := a.BaseColour.G + uint8(rand.Intn(randomAmount)+offset)
+		b := a.BaseColour.B + uint8(rand.Intn(randomAmount)+offset)
+		al := uint8(255)
+		a.Colours = append(a.Colours, color.RGBA{r, g, b, al})
+	}
 }
